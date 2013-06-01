2bbef628f4dce86873d38c270ea242177ef3bf32
Frigo <none@none>
2013-06-02 01:18:47 +0200

    - added command line interface

--- gamma/src/main/cpp/main.cpp
+++ gamma/src/main/cpp/main.cpp
@@ -1,11 +1,19 @@
 
+#include <cstdio>
+#include <cstdlib>
 #include <windows.h>
 
 #include "GammaSetter.h"
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    int argc;
+    LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+    double gamma = 1.0;
+    if( argc >= 2 && _wtof(argv[1]) != 0.0 ){
+        gamma = _wtof(argv[1]);
+    }
+
     GammaSetter setter;
-    setter.setGamma(0.6);
-    Sleep(3000);
-    setter.setGamma(1.0);
+    setter.setGamma(gamma);
 }
