934f51355ef852370525b1401f2b3c9a9545579c
Frigo <none@none>
2013-06-02 11:06:02 +0200

    - added a helpful message when started without params

--- gamma/src/main/cpp/main.cpp
+++ gamma/src/main/cpp/main.cpp
@@ -9,10 +9,16 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
     int argc;
     LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
 
-    double gamma = 1.0;
-    if( argc >= 2 && _wtof(argv[1]) != 0.0 ){
-        gamma = _wtof(argv[1]);
+    if( argc < 2 ){
+        printf("Sets the decoding gamma level of the entire desktop.\n");
+        printf("Usage: gamma gammalevel\n");
+        printf("Where gammalevel is a number between 0.23 and 4.45\n");
+        printf("For example: gamma 0.6\n");
+        return 1;
     }
+
+    double parsed = _wtof(argv[1]);
+    double gamma = parsed == 0.0 ? 1.0 : parsed;
 
     GammaSetter setter;
     setter.setGamma(1.0);
