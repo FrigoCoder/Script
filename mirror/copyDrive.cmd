d5c6944edec903e8e8ee70c166a82b958acdbbfd
Frigo <none@none>
2013-07-08 18:47:05 +0200

    - purge first, ask questions later

--- mirror/copyDrive.cmd
+++ mirror/copyDrive.cmd
@@ -1,3 +1,16 @@
+
 util\dosdev.exe %SHADOW_DRIVE% %1
-robocopy.exe %SHADOW_DRIVE%\ %DESTINATION_DRIVE%\ /zb /copyall /mir /xj /r:5 /ndl /np /unicode
+
+set copy_options=/nocopy /mir
+set file_selection_options=/xc /xn /xo /xl /xj
+set retry_options=/r:5
+set logging_options=/x /ndl /np /unicode
+robocopy.exe %SHADOW_DRIVE%\ %DESTINATION_DRIVE%\ %copy_options% %file_selection_options% %retry_options% %logging_options%
+
+set copy_options=/b /dcopy:t /copyall /secfix /timfix /mir
+set file_selection_options=/xj
+set retry_options=/r:5
+set logging_options=/x /ndl /np /unicode
+robocopy.exe %SHADOW_DRIVE%\ %DESTINATION_DRIVE%\ %copy_options% %file_selection_options% %retry_options% %logging_options%
+
 util\dosdev.exe -r -d %SHADOW_DRIVE%
