cb35c02d218a2a4fc78572b605bf6bab07463df4
Frigo <frigocoder@gmail.com>
2018-10-24 12:07:12 +0200

    Revert "Use subst instead of dosdev."
    This reverts commit 6ad7de17998702d23904a3fd758e04abe91c57f0.

--- mirror/copyDrive.cmd
+++ mirror/copyDrive.cmd
@@ -1,5 +1,5 @@
 
-subst %SHADOW_DRIVE% %1
+util\dosdev.exe %SHADOW_DRIVE% %1
 
 set copy_options=/nocopy /mir
 set file_selection_options=/xc /xn /xo /xl /xj
@@ -13,4 +13,4 @@ set retry_options=/r:5
 set logging_options=/x /ndl /np /unicode
 robocopy.exe %SHADOW_DRIVE%\ %DESTINATION_DRIVE%\ %copy_options% %file_selection_options% %retry_options% %logging_options%
 
-subst /d %SHADOW_DRIVE%
+util\dosdev.exe -r -d %SHADOW_DRIVE%
